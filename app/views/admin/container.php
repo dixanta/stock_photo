86ecd820667be38af8b551fac2bd266b6f9176f5
milanmertz <milanmertz@gmail.com>
2014-06-03 15:36:08 +0545

    view part

---
+++ app/views/admin/container.php
@@ -0,0 +1,4 @@
+<?php 
+$this->load->view($this->config->item('admin_folder').'/header');
+$this->load->view($this->config->item('admin_folder').'/content');
+$this->load->view($this->config->item('admin_folder').'/footer');
